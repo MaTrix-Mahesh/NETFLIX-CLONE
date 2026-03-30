c6a35ea6978eb9c15d55748c10e3d94828b9bd7b
mahesh sahu <shamahesh615@gmail.com>
2026-03-30 18:54:18 +0530

    Update Navbar.jsx

--- NETFLIX/src/components/navbar/Navbar.jsx
+++ NETFLIX/src/components/navbar/Navbar.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import '../navbar/Navbar.css'
+import logo from '../../assets/logo.png'
 function Navbar() {
   return (
     <div className=' navbar'>
-      navbar
+      <div className="navbar-left">
+        <img src={logo} alt="" />
+      </div>
+      <div className="navbar-right"></div>
+
     </div>
   )
 }
