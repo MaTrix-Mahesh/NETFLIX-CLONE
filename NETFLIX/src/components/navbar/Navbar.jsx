7d851b07b526901172976baaa46bf5bd3c838d9b
mahesh sahu <shamahesh615@gmail.com>
2026-03-30 19:18:46 +0530

    initial commit

--- NETFLIX/src/components/navbar/Navbar.jsx
+++ NETFLIX/src/components/navbar/Navbar.jsx
@@ -1,13 +1,38 @@
 import React from 'react'
 import '../navbar/Navbar.css'
 import logo from '../../assets/logo.png'
+import search_iocn from '../../assets/search_icon.svg'
+import bell_icon from '../../assets/bell_icon.svg'
+import profile_img from '../../assets/profile_img.png'
+import caret_icon from '../../assets/caret_icon.svg'
 function Navbar() {
   return (
     <div className=' navbar'>
       <div className="navbar-left">
         <img src={logo} alt="" />
+        <ul>
+            <li>Home </li>
+            <li>TV Show</li>
+            <li>Movies</li>
+            <li>New & Popular</li>
+            <li>My List</li>
+            <li>Browser By Language </li>
+        </ul>
       </div>
-      <div className="navbar-right"></div>
+      <div className="navbar-right">
+        <img src={search_iocn} className='icons' />
+        <p> children</p>
+        <img src={bell_icon} className='icons' />
+
+        <div className="navbar-Profile">
+            <img src={profile_img} alt=""  className=''/>
+            <img src={caret_icon} alt="" />
+            <div className="dropdown">
+                <p>sign out of Netflix</p>
+            </div>
+        </div>
+      </div>
+      
 
     </div>
   )
--- NETFLIX/src/pages/Home/Home.jsx
+++ NETFLIX/src/pages/Home/Home.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import '../Home/Home'
 import Navbar from '../../components/navbar/Navbar'
-
+import hero_banner from '../../assets/hero_banner.jpg'
+import hero_banner from '../../assets/hero_banner.jpg'
+import hero_tittle from '../../assets/hero_tittle.png'
 function Home() {
   return (
     <div  className='home'>
       <Navbar/>
+
+      <div className="hero">
+        <img src={hero_banner} alt="" className='banner_img' />
+        <div className="hero-caption">
+            <img src={hero_tittle} alt="" />
+            
+        </div>
+      </div>
     </div>
   )
 }
