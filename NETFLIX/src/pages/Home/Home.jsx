2aba9ddb232fc0895dec165a4e509b97f17ecc3e
mahesh sahu <shamahesh615@gmail.com>
2026-03-30 20:02:26 +0530

    initial commit

--- NETFLIX/src/pages/Home/Home.jsx
+++ NETFLIX/src/pages/Home/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
-import '../Home/Home'
+import '../Home/Home.css'
 import Navbar from '../../components/navbar/Navbar'
 import hero_banner from '../../assets/hero_banner.jpg'
-import hero_banner from '../../assets/hero_banner.jpg'
-import hero_tittle from '../../assets/hero_tittle.png'
+import hero_title from '../../assets/hero_title.png'
+
+
 function Home() {
   return (
     <div  className='home'>
@@ -12,8 +13,8 @@ function Home() {
       <div className="hero">
         <img src={hero_banner} alt="" className='banner_img' />
         <div className="hero-caption">
-            <img src={hero_tittle} alt="" />
-            
+            <img src={hero_title} alt="" />
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem dolorum, quis laudantium optio unde deserunt ea incidunt tenetur eaque delectus perspiciatis maiores aliquid reprehenderit id. Possimus modi sint quae neque.</p>
         </div>
       </div>
     </div>
