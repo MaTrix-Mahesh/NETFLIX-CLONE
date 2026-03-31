a2fbb07e5a76f7c3b0997ec4e1afd1a96e4f0a1b
mahesh sahu <shamahesh615@gmail.com>
2026-03-31 17:44:26 +0530

    initial commit

--- NETFLIX/src/components/titleCard/Tittlecard.jsx
+++ NETFLIX/src/components/titleCard/Tittlecard.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 import '../titleCard/Tittlecard.css'
+import cards_data from '../../assets/cards/Cards_data'
+
 function Tittlecard() {
   return (
-    <div    className='tittlecards'>
-      
+    <div className='tittle-cards'>
+
+      <h2>Popular on Netflix</h2>
+      <div className="card-list">
+        {
+          cards_data.map((card, index)=>{
+            return <div className="card"  key={index}>
+              <img src={card.image} alt="" />
+              <p>{card.name}</p>
+            </div>
+          })
+        }
+      </div>
     </div>
   )
 }
--- NETFLIX/src/pages/Home/Home.jsx
+++ NETFLIX/src/pages/Home/Home.jsx
@@ -5,6 +5,9 @@ import hero_banner from '../../assets/hero_banner.jpg'
 import hero_title from '../../assets/hero_title.png'
 import play_icon from '../../assets/play_icon.png'
 import info_icon from '../../assets/info_icon.png'
+import Tittlecard from '../../components/titleCard/Tittlecard'
+
+ 
 function Home() {
   return (
     <div  className='home'>
@@ -20,6 +23,7 @@ function Home() {
             <button><img src={info_icon} alt="" className='btn dark-btn' />More Info</button>
 
         </div>
+        <Tittlecard/>
         </div>
         
       </div>
